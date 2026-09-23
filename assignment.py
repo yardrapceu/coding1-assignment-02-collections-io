e95e6eb41ab4460c6efd302dfa29f2c87108d259
Yaryna Drapaylo <drapaylo_yaryna@student.ceu.edu>
2026-09-23 17:18:54 +0200

    added comments

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
-drink = "tea"
-sales = {"tea": 3, "coffee": 5}
-sold = sales[drink]
-sold = sold + 1
-print(drink)
-print(sold)
+drink = "tea" # Assigning a string value to the variable 'drink'
+sales = {"tea": 3, "coffee": 5} # Defining the ?list? with two variables and their associated values
+sold = sales[drink] # Getting the value associated with 'drink', which is string 'tea' in our case
+sold = sold + 1 # Editing the previously defined value 'sold' by adding 1 to it
+print(drink) # Expected output is "tea"
+print(sold) # Expected output is either 4 or error (idk what value is taken from the ?list?)
